e1df0ecb151cf3efe0c73eb5397a23b7ee84a513
Pibborn <matcerrato@gmail.com>
2014-11-12 16:51:43 +0100

    nuovo robusto testing

--- FingerLib/src/main/java/mattia/fingerlib/DirectoryInfo.java
+++ FingerLib/src/main/java/mattia/fingerlib/DirectoryInfo.java
@@ -4,12 +4,13 @@ package mattia.fingerlib;
  * Created by mattia cerrato on 20/10/14.
  */
 public class DirectoryInfo {
-    private final String audioDirPath = "/Users/tesi/Desktop/radio/";
+    private String audioDirPath = "/Users/tesi/Desktop/radio/";
     private final String logDirPath = "/Users/tesi/Desktop/FingerLibrary/logs/";
     private final String rDirPath = "/Users/tesi/Desktop/FingerLibrary/rlogs/";
     private final String imgDirPath = "/Users/tesi/Desktop/FingerLibrary/img/";
     private final String peakDirPath = "/Users/tesi/Desktop/FingerLibrary/peaks/";
-    private final String otaDirPath = "/Users/tesi/Desktop/ota/";
+    private String otaDirPath = "/Users/tesi/Desktop/ota/";
+    private final String resultDirPath = "/Users/tesi/Desktop/test-results/";
     private static DirectoryInfo instance;
 
     private DirectoryInfo() {}
@@ -43,5 +44,17 @@ public class DirectoryInfo {
 
     public String getOtaDirPath() {
         return otaDirPath;
+    }
+
+    public String getResultDirPath() {
+        return resultDirPath;
+    }
+
+    public void setAudioDirPath(String audioDirPath) {
+        this.audioDirPath = audioDirPath;
+    }
+
+    public void setOtaDirPath(String otaDirPath) {
+        this.otaDirPath = otaDirPath;
     }
 }
--- FingerLib/src/main/java/mattia/fingerlib/FingerTest.java
+++ FingerLib/src/main/java/mattia/fingerlib/FingerTest.java
@@ -5,13 +5,15 @@ import com.google.common.collect.ListMultimap;
 
 import org.apache.commons.math3.stat.descriptive.rank.Percentile;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.util.Collections.max;
-import static java.util.Collections.sort;
 
 /**
  * Created by mattia cerrato on 29/09/14.
@@ -22,83 +24,112 @@ public class FingerTest {
 
     public static void main(String[] args) {
 
+        TestLogParser testLogParser = new TestLogParser();
+        List<TestCase> testList = null;
+        try {
+            testList = testLogParser.generateTestList(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        PrintWriter testResultWriter = null;
+
+        //il nome del file-risultato di log è la descrizione del primo file di test
+        String resultFilePath = testList.get(0).getDescription()+".log";
+        try {
+            testResultWriter = new PrintWriter(new BufferedWriter(new FileWriter(resultFilePath, true)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         DirectoryInfo dirInfo = DirectoryInfo.getInstance();
 
-        PlotImageGenerator plotter = new PlotImageGenerator(dirInfo.getImgDirPath(), dirInfo.getAudioDirPath(), dirInfo.getLogDirPath());
-        plotter.setAudioPath(dirInfo.getAudioDirPath());
-        plotter.setLogPath(dirInfo.getLogDirPath());
 
-        PeakLogger logger = new PeakLogger(dirInfo.getLogDirPath(), dirInfo.getPeakDirPath(), dirInfo.getrDirPath());
+        for(TestCase test : testList) {
+            dirInfo.setAudioDirPath(test.getAudioPath());
+            dirInfo.setOtaDirPath(test.getOtaPath());
 
-
-        File audioDirFile = new File(dirInfo.getAudioDirPath());
-        File[] audioDir = audioDirFile.listFiles();
-
-        File otaDirFile = new File(dirInfo.getOtaDirPath());
-        File[] otaDir = otaDirFile.listFiles();
+//            generazione plotter (non lo uso più)
+//            PlotImageGenerator plotter = new PlotImageGenerator(dirInfo.getImgDirPath(), dirInfo.getAudioDirPath(), dirInfo.getLogDirPath());
+//            plotter.setAudioPath(dirInfo.getAudioDirPath());
+//            plotter.setLogPath(dirInfo.getLogDirPath());
 
 
-        List<Double> dbMatchList = new ArrayList<Double>();
-        List<Double> otaMatchList = new ArrayList<Double>();
 
-        ListMultimap<String, TimePair> timeMatchMap = ArrayListMultimap.create();
-        ListMultimap<Integer, Integer> peakMap = ArrayListMultimap.create();
+            PeakLogger logger = new PeakLogger(dirInfo.getLogDirPath(), dirInfo.getPeakDirPath(), dirInfo.getrDirPath());
 
-        String match = "";
+            File audioDirFile = new File(dirInfo.getAudioDirPath());
+            File[] audioDir = audioDirFile.listFiles();
 
-        //generazione songmap
-        SongMap songMap = SongMap.getInstance();
-        songMap.generateArr();
+            File otaDirFile = new File(dirInfo.getOtaDirPath());
+            File[] otaDir = otaDirFile.listFiles();
 
-        FingerprintGenerator finGen = new FingerprintGenerator();
 
-        int db_size = 0;
-        //generazione fingerprint delle tracce-db
-        for (File track : audioDir) {
-            if (track.getName().compareTo(".DS_Store") == 0) continue;
-            finGen.setTrack(track);
-            peakMap = finGen.generateFingerprints(true, false);
-            db_size++;
-        }
-        //generazione fingerprint delle tracce-ota (solitamente una!)
-        for (File ota : otaDir) {
-            if (ota.getName().compareTo(".DS_Store") == 0) continue;
-            finGen.setTrack(ota);
-            timeMatchMap = finGen.generateFingerprints(true, true);
-        }
-        //generazione score delle tracce-db rispetto all'ota
-        int[] scores = new int[db_size];
-        String[] trackNames = new String[db_size];
-        boolean[] histoPeak = new boolean[db_size];
-        int max = 0;
-        int i = 0;
-        for (File track : audioDir) {
-            if (track.getName().compareTo(".DS_Store") == 0) continue;
-            trackNames[i] = track.getName();
-            finGen.setTrack(track);
-            int score = finGen.trackScore();
-            scores[i] = score;
-            boolean hasPeak = finGen.findHistogramPeak(true);
-            histoPeak[i] = hasPeak;
-            if (score > max) {
-                max = score;
-                match = track.getName();
+            List<Double> dbMatchList = new ArrayList<Double>();
+            List<Double> otaMatchList = new ArrayList<Double>();
+
+            ListMultimap<String, TimePair> timeMatchMap = ArrayListMultimap.create();
+            ListMultimap<Integer, Integer> peakMap = ArrayListMultimap.create();
+
+            String match = "";
+
+            //generazione songmap
+            SongMap songMap = SongMap.getInstance();
+            songMap.generateArr();
+
+            FingerprintGenerator finGen = new FingerprintGenerator();
+
+            String expectedMatch = test.getExpectedMatch();
+            boolean hasAMatch = false;
+            int db_size = 0;
+            //generazione fingerprint delle tracce-db
+            for (File track : audioDir) {
+                if (track.getName().compareTo(".DS_Store") == 0) continue;
+                if (track.getName().contains(".sh")) continue;
+                if (expectedMatch.compareTo(track.getName()) == 0) hasAMatch = true; //allora un match andrebbe trovato
+                finGen.setTrack(track);
+                peakMap = finGen.generateFingerprints(true, false);
+                db_size++;
             }
-            i++;
-        }
-        double[] sorted_scores = new double[db_size];
-        for (int j = 0; j < db_size; j++) {
-            sorted_scores[j] = (double) scores[j];
-        }
-        Arrays.sort(sorted_scores);
-        double next_to_max = sorted_scores[db_size-2];
-        double difference = max - next_to_max;
-        difference *= difference;
-        System.out.println("quadrato della differenza: "+difference);
-        Percentile percentileCalculator = new Percentile();
-        double filter_value = percentileCalculator.evaluate(sorted_scores, 80);
-        System.out.println("80mo percentile dei punteggi: "+filter_value);
+            //generazione fingerprint delle tracce-ota (solitamente una!)
+            for (File ota : otaDir) {
+                if (ota.getName().compareTo(".DS_Store") == 0) continue;
+                finGen.setTrack(ota);
+                timeMatchMap = finGen.generateFingerprints(true, true);
+            }
+            //generazione score delle tracce-db rispetto all'ota
+            int[] scores = new int[db_size];
+            String[] trackNames = new String[db_size];
+            boolean[] histoPeak = new boolean[db_size];
+            int max = 0;
+            int i = 0;
+            for (File track : audioDir) {
+                if (track.getName().compareTo(".DS_Store") == 0) continue;
+                if (track.getName().contains(".sh")) continue;
+                trackNames[i] = track.getName();
+                finGen.setTrack(track);
+                int score = finGen.trackScore();
+                scores[i] = score;
+                boolean hasPeak = finGen.findHistogramPeak(true);
+                histoPeak[i] = hasPeak;
+                if (score > max) {
+                    max = score;
+                    match = track.getName();
+                }
+                i++;
+            }
+            double[] sorted_scores = new double[db_size];
+            for (int j = 0; j < db_size; j++) {
+                sorted_scores[j] = (double) scores[j];
+            }
+            Arrays.sort(sorted_scores);
+            double next_to_max = sorted_scores[db_size-2];
+            double difference = max - next_to_max;
+            difference *= difference;
+            System.out.println("quadrato della differenza: "+difference);
+            Percentile percentileCalculator = new Percentile();
+            double filter_value = percentileCalculator.evaluate(sorted_scores, 80);
+            System.out.println("80mo percentile dei punteggi: "+filter_value);
 //        int times = 0;
 //        for (int j = 0; j < db_size; j++) {
 //            if (scores[j] > filter_value) {
@@ -114,6 +145,26 @@ public class FingerTest {
                 match = "no match!";
             }
 //        }
-        System.out.println("previsione di match: "+ match);
+            System.out.println("previsione di match: "+ match);
+            File outTestFile = new File(test.getOutPath()+test.getTestName());
+            PrintWriter writer = null;
+            if (hasAMatch && (match.compareTo(expectedMatch) == 0)) {
+                System.out.println(test.getTestName() + ": success (found the right match)");
+                testResultWriter.println(test.getTestName()+": success (found the right match)");
+            }
+            else if(!hasAMatch && (match.compareTo("no match!") == 0)) {
+                System.out.println(test.getTestName() + ": success (no match)");
+                testResultWriter.println(test.getTestName()+": success (no match)");
+            }
+            else if(!hasAMatch && (match.compareTo("no match!") != 0)) {
+                System.out.println(test.getTestName() + ": failure (false positive)");
+                testResultWriter.println(test.getTestName()+": failure (false positive)");
+            }
+            else if(hasAMatch && (match.compareTo(expectedMatch) != 0)) {
+                System.out.println(test.getTestName() + ": failure (matched the wrong track)");
+                testResultWriter.println(test.getTestName()+": failure (matched the wrong track)");
+            }
+        }
+        testResultWriter.close();
     }
 }
